d30b1f6aa890e05576d82d8d8d9f9c0fe9adc339
Jerry Chen <jchen0022@gmail.com>
2017-05-14 01:20:14 -0700

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,53 @@
+module ApplicationHelper
+  def vice_pictures
+    vices = [
+        {
+          name: "DRINKING",
+          label: 2,
+          title: "Drinking",
+          image: "drinking.jpg"
+
+        },
+        {
+          name: "DRUGS",
+          label: 3,
+          title: "Drugs",
+          image: "drugs.jpg"
+
+        },
+        {
+          name: "SMOKING",
+          label: 4,
+          title: "Smoking",
+          image: "smoking.jpg"
+        },
+        {
+          name: "GAMBLING",
+          label: 5,
+          title: "Gambling",
+          image: "gambling.jpg"
+
+        },
+        {
+          name: "SEX&LOVE",
+          label: 6,
+          title: "Sex & Love",
+          image: "sex&love.jpg"
+
+        },
+        {
+          name: "EATING",
+          label: 8,
+          title: "Eating",
+          image: "eating.jpg"
+
+        }
+      ]
+    return vices
+  end
+
+  def get_companies
+    companies = ["cbs", "cedars", "crains", "fortune", "huffington", "michigan", "mobihealth", "newmexico"]
+    return companies
+  end
+end
