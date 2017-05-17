efe9804e647202e34c8f28476c7a37525e21ad4e
Jerry Chen <jchen0022@gmail.com>
2017-05-16 17:52:44 -0700

    removed outline

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,10 @@
 $(document).on('ready', function() {
     $(".main-slider").slick({
         autoplay: true,
-        dots: true,
-        infinite: false,
         centerMode: true,
         centerPadding: '200px',
+        dots: true,
+        infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
     });
