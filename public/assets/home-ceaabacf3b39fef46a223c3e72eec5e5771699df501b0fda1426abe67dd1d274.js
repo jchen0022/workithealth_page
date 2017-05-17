c600239df80414f0ea5cd193ccea6d432ce852ee
Jerry Chen <jchen0022@gmail.com>
2017-05-16 18:15:41 -0700

    Precompile assets

---
+++ public/assets/home-ceaabacf3b39fef46a223c3e72eec5e5771699df501b0fda1426abe67dd1d274.js
@@ -0,0 +1,1 @@
+$(document).on("ready",function(){$(".main-slider").slick({autoplay:!0,centerMode:!0,centerPadding:"200px",dots:!0,focusOnSelect:!0,infinite:!1,slidesToShow:1,slidesToScroll:1,swipeToSlide:!0})});
